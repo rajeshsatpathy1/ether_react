24d9943a18aff947fbc69a8f0ec28f5fe13bf895
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-09-12 20:01:17 -0500

    Add functionality for contract to enter lottery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import web3 from './web3';
-import lottery from './lottery';
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
 import React from "react";
 
 class App extends React.Component {
@@ -8,15 +8,31 @@ class App extends React.Component {
     manager: "",
     players: [],
     balance: "",
+    value: "",
   };
 
   async componentDidMount() {
     const [manager, players, balance] = await Promise.all([
       lottery.methods.manager().call(),
       lottery.methods.getPlayers().call(),
-      web3.eth.getBalance(lottery.options.address)
+      web3.eth.getBalance(lottery.options.address),
     ]);
     this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async(event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success . . ."});
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether")
+    });
+
+    this.setState({ message: "You have been successfully entered into the lottery!" });
   }
 
   render() {
@@ -28,7 +44,26 @@ class App extends React.Component {
           {this.state.players.length} people, competing to win{" "}
           {web3.utils.fromWei(this.state.balance, "ether")} ether!
         </p>
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter: </label>
+            <input
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+        
+        <hr/>
+
+        <h1>{ this.state.message }</h1>
       </div>
+
     );
   }
 }
